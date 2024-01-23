7bc58efd5ccf861d35096d05ab817570d377ecd1
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-01-23 20:46:01 +0530

    Hero Dashboard

--- client/src/components/Dashboard/MyInfo.jsx
+++ client/src/components/Dashboard/MyInfo.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const MyInfo = () => {
   return (
-    <div>MyInfo</div>
+    <div className='myinfo'>
+        <div className='myinfocards'>
+        <h6 style={{color: "#FF5D01"}} className='myinfocard'>
+            <span className='amount'>12</span>
+            <span className='label'> Quizzes Created</span>
+        </h6>
+
+        <h6 style={{color: "#60B84B"}} className='myinfocard'>
+            <span className='amount'>121</span>
+            <span className='label'> Questions Created</span>
+        </h6>
+
+        <h6 style={{color: "#5076FF"}} className='myinfocard'>
+            <span className='amount'>1.4K</span>
+            <span className='label'> Total Impressions</span>
+        </h6>
+        </div>
+    </div>
   )
 }
 
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../styles/Dashboard.css"
 import MyInfo from '../components/Dashboard/MyInfo'
 
 const Dashboard = () => {

c7e22a608154c01c3afffb8ddae28eee20ce68db
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-01-28 00:08:37 +0530

    Backend Boilerplate

---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+const expess = require("express");
+const router = expess.Router();
+
+// const auth = require("./auth");
+// const product = require("./product");
+// const view = require("./view");
+
+const r = () => {
+//   auth(router);
+//   product(router);
+//   view(router);
+  return router;
+};
+
+module.exports = r;
